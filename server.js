33832b64b9db2dc648be7370578f17b208585b57
Eizeiky777 <ueki.winchester23@gmail.com>
2020-07-16 20:48:04 +0700

    fix

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser=require('body-parser');
 const router = require("./routes");
 var cors = require('cors');
 const app = express();
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 
 
