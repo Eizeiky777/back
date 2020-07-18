5a9b7324c3793c6e315dae1f23e73e5f786b9e7e
Eizeiky777 <ueki.winchester23@gmail.com>
2020-07-18 12:15:02 +0700

    fix cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 
-const express = require("express");
-const bodyParser=require('body-parser');
+
+//const bodyParser=require('body-parser');
 const router = require("./routes");
 var cors = require('cors');
+const compression = require('compression');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -11,17 +13,17 @@ const port = process.env.PORT || 5000;
 app.enable('trust proxy');
 
 app.use(express.json());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+// app.use(bodyParser.json());
+app.use(express.urlencoded({extended: true}));
 // app.use(express.static('public'));
 
 
 
 app.use(cors());
 app.options('*', cors());
+app.use(compression());
 
 app.use('/public', express.static('public/img'));
-
 app.use("/api/v1", router);
 // app.use('/static', express.static(path.join(__dirname,'public')));
 
