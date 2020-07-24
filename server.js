e4e37efbe9876906d7acb7b94cd07d6f68a39dc3
Eizeiky777 <ueki.winchester23@gmail.com>
2020-07-24 17:18:05 +0700

    fix cors

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.options('*', cors());
-app.enable('trust proxy');
+// app.options('*', cors());
+// app.enable('trust proxy');
 
 app.use(express.json());
 // app.use(bodyParser.json());
@@ -18,7 +18,7 @@ app.use(express.urlencoded({extended: true}));
 // app.use(express.static('public'));
 
 
-app.use(cors({origin: true, credentials: true, methods: ['GET', 'PUT', 'POST', 'DELETE', 'PATCH', 'HEAD'] }));
+app.use(cors({origin: '*', credentials: true, methods: ['GET', 'PUT', 'POST', 'DELETE', 'PATCH', 'HEAD'] }));
 app.use(compression());
 
 app.use('/public', express.static('public/img'));
